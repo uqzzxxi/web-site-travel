86e68361d8076cebf216c785f7616fdc20d3a054
uqzzxxi <askar.ulzhan@narxoz.kz>
2024-05-21 01:19:53 +0500

    Delete scriptcruiz.js

--- scriptcruiz.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('booking-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Ваш круиз успешно забронирован! Круиз: ' + document.getElementById('cruise').value +
-          ', Тип каюты: ' + document.getElementById('cabin-type').value +
-          ', Пассажиры: ' + document.getElementById('passengers').value +
-          ', Дата: ' + document.getElementById('date').value);
-});
